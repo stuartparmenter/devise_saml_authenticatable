a369a435f9ef704b916405820f7430365bd27c72
Stuart Parmenter <pavlov@pavlov.net>
2017-02-01 11:01:43 -0800

    x

--- lib/devise_saml_authenticatable/default_idp_entity_id_reader.rb
+++ lib/devise_saml_authenticatable/default_idp_entity_id_reader.rb
@@ -1,11 +1,10 @@
 module DeviseSamlAuthenticatable
   class DefaultIdpEntityIdReader
-    include DeviseSamlAuthenticatable::SamlConfig
     def self.entity_id(params)
       if params[:SAMLRequest]
-        OneLogin::RubySaml::SloLogoutrequest.new(params[:SAMLRequest], settings: saml_config).issuer
+        OneLogin::RubySaml::SloLogoutrequest.new(params[:SAMLRequest], settings: Devise.saml_config).issuer
       elsif params[:SAMLResponse]
-        OneLogin::RubySaml::Response.new(params[:SAMLResponse], settings: saml_config).issuers.first
+        OneLogin::RubySaml::Response.new(params[:SAMLResponse], settings: Devise.saml_config).issuers.first
       end
     end
   end
